307146e0e61349d5669afa7d449c2c6d96d9841a
ovclid <53159064+ovclid@users.noreply.github.com>
2024-09-03 15:46:47 +0900

    Update market_info_South.js

--- market_info_South.js
+++ market_info_South.js
@@ -1,5 +1,12 @@
 // 보은, 옥천, 영동
 
 var polygonPath_South = {
-    
+    "영동전통시장" : [ 
+        new kakao.maps.LatLng(36.17845541891501, 127.7750884896691 ), 
+        new kakao.maps.LatLng(36.17777260593731, 127.7736294121847 ), 
+        new kakao.maps.LatLng(36.17658453393349, 127.7737971636314 ), 
+        new kakao.maps.LatLng(36.17580624993743, 127.7742684394236 ), 
+        new kakao.maps.LatLng(36.1740116737253, 127.7752048587474 ), 
+        new kakao.maps.LatLng(36.17537092097049, 127.7775726975752 ), 
+        ],
 }
